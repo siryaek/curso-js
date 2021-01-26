6047827138b282ce8fe12c5fb14bd744267f4228
sir.ye <67657401+siryaek@users.noreply.github.com>
2021-01-26 20:18:26 -0300

    Create matematica.js

---
+++ Modulo B/matematica.js
@@ -0,0 +1,24 @@
+var conta1 = 1 + 1
+console.log(conta1)
+
+/* Como esperado, ele irá dar 2 no console, pois 1 + 1 seria 2 e não onze né mkkkkkk */
+
+var conta2 = 1 / 1
+console.log(conta2)
+
+/* Como esperado, ele irá dar 1 pois 1 divido por 1 seria 1! */
+
+var conta3 = 1 - 1
+console.log(conta3)
+
+/* Pelo visto irá dar 0, pois 1 menos 1 é 0! */
+
+var conta4 = 1 * 1
+console.log(conta4)
+
+/* como esperado, ele irá dar 1 pois 1 vezes 1 seria 1! */
+
+var conta5 = 8 % 3
+console.log(conta5)
+
+/* 8 % 3 (retorna 2; como três cabe duas vezes em 8, deixando 2 como resto.)
